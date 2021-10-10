b3d80bfc26112ed125358943c1aae7cca8ba99b3
Abdelmn3m mohamed <abdel_moneim122@hotmail.com>
2021-10-10 14:06:50 +0200

    collect all desgin in main route

--- src/app/formual-tree/formual-tree.module.ts
+++ src/app/formual-tree/formual-tree.module.ts
@@ -10,6 +10,10 @@ import { FormulaGroupComponent } from './components/formual-tree/formula-group/f
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EnumToArrayPipe } from '../pipe/enumToArray.pipe';
 import { ClinicalGroupTestComponent } from './components/formual-tree/formula-group/rule/controls/clinical-group-test/clinical-group-test.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { AddRuleButtonComponent } from './components/formual-tree/formula-group/buttons/add-rule-button/add-rule-button.component';
 
 
 @NgModule({
@@ -20,13 +24,20 @@ import { ClinicalGroupTestComponent } from './components/formual-tree/formula-gr
     RuleComponent,
     FormulaGroupComponent,
     EnumToArrayPipe,
-    ClinicalGroupTestComponent
+    ClinicalGroupTestComponent,
+    AddRuleButtonComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
     ReactiveFormsModule,
     FormualTreeRoutingModule
+  ],
+  exports: [
+    FormualTreeComponent
   ]
 })
 export class FormualTreeModule { }
